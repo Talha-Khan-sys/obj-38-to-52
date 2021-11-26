a475a12561d51db9a8408fdfff52d8b16c3709ea
Muhammad Talha Khan <hdmoviez1234@gmail.com>
2021-11-26 23:48:09 +0500

    obj 38 to 52

---
+++ app.js
@@ -0,0 +1,20 @@
+// function purchase(){
+//     alert("Thanks From Purchasing Our Website!")
+// }
+
+
+
+// function link(){
+//     alert("Thanks for visiting our Website!")
+// }
+
+
+
+
+
+// function setnewimage(){
+//     document.getElementById("img1").src="./images/bike.jpg";
+// }
+// function setoldimage(){
+//     document.getElementById("img1").src="./images/car.jpg"
+// }
